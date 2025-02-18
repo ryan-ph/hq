20e50c6dc9621f7ada39ffd40315ae8e8c6f1eda
Ryan Pham <phamcake@gmail.com>
2025-02-18 11:04:35 +0900

    filter: support parsing numeric index
    The intention of this is to eventually allow folks to query and delete
    specific indicies within a list type. Additionally, this adds futher
    restrictions to prevent parsing properties and names that start with
    digits which is not allowed in HCL.

--- src/filter/parser.rs
+++ src/filter/parser.rs
@@ -1,4 +1,7 @@
-use pest::Parser;
+use pest::{
+    error::{Error, ErrorVariant},
+    Parser,
+};
 use pest_derive::Parser;
 
 use super::error::FilterError;
@@ -15,10 +18,12 @@ pub struct Filter {}
 /// * the name "baz"
 #[derive(Clone, Debug, PartialEq)]
 pub struct Field {
-    /// an attribute or block name
+    /// an attribute, block, or object name
     pub name: String,
-    /// block labels
+    /// block labels or object keys
     pub labels: Vec<String>,
+    /// a list index
+    pub index: Option<usize>,
 }
 
 impl Field {
@@ -26,6 +31,7 @@ impl Field {
         Field {
             name: name.to_string(),
             labels: Vec::new(),
+            index: None,
         }
     }
 
@@ -33,6 +39,15 @@ impl Field {
         Field {
             name: name.to_string(),
             labels: labels.iter().map(|label| label.to_string()).collect(),
+            index: None,
+        }
+    }
+
+    pub fn indexed(name: &str, index: usize) -> Self {
+        Field {
+            name: name.to_string(),
+            labels: Vec::new(),
+            index: Some(index),
         }
     }
 }
@@ -46,6 +61,7 @@ pub fn parse_filter(input: &str) -> Result<Vec<Field>, Box<FilterError<Rule>>> {
     for pair in pairs {
         let mut name = String::new();
         let mut labels = Vec::new();
+        let mut index: Option<usize> = None;
 
         let inner_pairs = pair.into_inner();
         for inner in inner_pairs {
@@ -61,11 +77,38 @@ pub fn parse_filter(input: &str) -> Result<Vec<Field>, Box<FilterError<Rule>>> {
                 Rule::label => {
                     labels.push(inner.as_str().to_owned());
                 }
-                _ => {}
+                Rule::numeric_index => {
+                    if let Ok(i) = inner.as_str().parse::<usize>() {
+                        index = Some(i);
+                    } else {
+                        return Err(Error::new_from_span(
+                            ErrorVariant::ParsingError {
+                                positives: Vec::new(),
+                                negatives: vec![inner.as_rule()],
+                            },
+                            inner.as_span(),
+                        )
+                        .into());
+                    }
+                }
+                _ => {
+                    return Err(Error::new_from_span(
+                        ErrorVariant::CustomError {
+                            message: "not implemented".to_string(),
+                        },
+                        inner.as_span(),
+                    )
+                    .into())
+                }
             }
         }
+
         if !name.is_empty() {
-            fields.push(Field { name, labels });
+            fields.push(Field {
+                name,
+                labels,
+                index,
+            });
         }
     }
     Ok(fields)
@@ -73,38 +116,47 @@ pub fn parse_filter(input: &str) -> Result<Vec<Field>, Box<FilterError<Rule>>> {
 
 #[cfg(test)]
 mod tests {
-    use std::error::Error;
-
     use super::*;
 
     #[test]
-    fn name_filter() -> Result<(), Box<dyn Error>> {
+    fn name_filter() {
         let input = ".a_name";
         let expected = vec![Field::new("a_name")];
-        let fields = parse_filter(input)?;
+        let fields = parse_filter(input).expect("failed to parse");
         assert_eq!(expected, fields);
-        Ok(())
     }
 
     #[test]
-    fn label_filter() -> Result<(), Box<dyn Error>> {
+    fn label_filter() {
         let input = ".a_name{\"a_label\"}";
         let expected = vec![Field::labeled("a_name", &["a_label"])];
-        let fields = parse_filter(input)?;
+        let fields = parse_filter(input).expect("failed to parse");
         assert_eq!(expected, fields);
-        Ok(())
     }
 
     #[test]
-    fn traversal_filter() -> Result<(), Box<dyn Error>> {
+    fn index_filter() {
+        let input = ".a_name[0]";
+        let expected = vec![Field::indexed("a_name", 0)];
+        let fields = parse_filter(input).expect("failed to parse");
+        assert_eq!(expected, fields);
+    }
+
+    #[test]
+    fn traversal_filter() {
         let input = ".a_name{\"a_label\"}.another_name{\"another_label\"}.third_name";
         let expected = vec![
             Field::labeled("a_name", &["a_label"]),
             Field::labeled("another_name", &["another_label"]),
             Field::new("third_name"),
         ];
-        let fields = parse_filter(input)?;
+        let fields = parse_filter(input).expect("failed to parse");
         assert_eq!(expected, fields);
-        Ok(())
+    }
+
+    #[test]
+    fn name_starting_with_digit_should_err() {
+        let input = ".00asdf";
+        parse_filter(input).expect_err("succesfully parsed invalid name");
     }
 }
